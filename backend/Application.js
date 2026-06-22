1195246117657d0edf3c188d9e682a6c746db9c4
Rakshitha HN <rakshug73@gmail.com>
2026-06-22 11:14:08 +0530

    Add files via upload

---
+++ backend/Application.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const applicationSchema = new mongoose.Schema({
+  jobId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Job",
+    required: true,
+  },
+  applicantName: {
+    type: String,
+    required: true,
+  },
+  applicantEmail: {
+    type: String,
+    required: true,
+  },
+  appliedDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Application", applicationSchema);
